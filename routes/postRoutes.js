a09e34441a8c1b54869c37b184a25a4bca11e77b
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-11-02 10:27:48 -0700

    added first 3 methods, forgot to commit. woops

---
+++ routes/postRoutes.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const db = require("../data/db");
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  const newPost = req.body;
+
+  if (!newPost.title || !newPost.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    db.insert(newPost)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(() => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  }
+});
+
+
+
+router.post("/:id/comments", (req, res) => {
+  const commentInfo = { ...req.body, post_id: req.params.id };
+  const id = req.params.id;
+
+  if (!commentInfo.text) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide text for the comment." });
+  } else {
+    db.findById(id)
+      .then(post => {
+        if (post.length > 0) {
+          db.insertComment(commentInfo)
+            .then(comment => {
+              res.status(201).json(comment);
+            })
+            .catch(err => {
+              res.status(500).json({
+                error:
+                  "There was an error while saving the comment to the database",
+                err
+              });
+            });
+        } else {
+          res
+            .status(404)
+            .json({
+              message: "The post with the specified ID does not exist."
+            });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the comment to the database",
+          err
+        });
+      });
+  }
+});
+
+
+
+router.get("/", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+module.exports = router;
